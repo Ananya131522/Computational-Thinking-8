0addb8e78b08e932a035e2480afb35905b23987d
Ananya131522 <ananyaiyer@seattleacademy.org>
2025-02-12 17:21:49 +0000

    this is my dodge collect game

--- Projects/dodge_collect.py
+++ Projects/dodge_collect.py
@@ -3,22 +3,44 @@ import codesters, random
 from codesters import StageClass
 stage = StageClass ()
 stage.disable_floor()
-player = codesters.Sprite("singer")
+player = codesters.Sprite("singer (1)", 0,-150)
 player.set_size(0.2)
 stage.set_background("erastour")
 #starting object speed
-object_speed = -20
+object_speed = -2
 #point tracker
 points = 0
 #Section 2: Objects
 def falling_object():
     global object_speed, object_speed, points
     
-    if points != 15:
+    if points != 30:
         x = random.randint(-250,250)
-        y = 250
-        object = codesters.Sprite("butterfly", x, y)
-        object.set_size(0.002)
+        y = random.randint(-250,250)
+        object = codesters.Sprite("butterfly (1)", 0, 600)
+        object.set_size(0.2)
+        object.go_to(x,y)
         object.set_y_speed(object_speed)
 
-stage.event_interval(falling_object, 1)
+stage.event_interval(falling_object, 5)
+# #Section 3: Collision
+def collision(player,object):
+    global points
+    if object.get_image_name() == "butterfly (1)":
+
+        stage.remove_sprite(object)
+        points += 1
+        if points == 30:
+            player.say(f"Good job! Now go get on stage!")
+        else:
+            player.say(f"{points}points", 0.5)
+player.event_collision(collision)
+#Section 4: Controls
+
+#Right Key
+def go_right():
+    player.move_right(10)
+player.event_key("right",go_right)
+def go_left():
+    player.move_left(10)
+player.event_key("left", go_left)
